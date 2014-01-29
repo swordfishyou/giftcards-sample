0e0e6e39b8885f78802ee10fb2da59c4787c1f8a
Anatoly Tukhtarov <anatoly.tukhtarov@yalantis.com>
2014-01-29 16:32:17 +0200

    Dummy UI for editor view controller

---
+++ cards/cards/Classes/View Controllers/GCEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCEditorViewController.h
+//  cards
+//
+//  Created by Anatoly Tukhtarov on 1/29/14.
+//  Copyright (c) 2014 Anatoly Tukhtarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCEditorViewController : UIViewController
+
+@end
---
+++ cards/cards/Classes/View Controllers/GCEditorViewController.m
@@ -0,0 +1,17 @@
+//
+//  GCEditorViewController.m
+//  cards
+//
+//  Created by Anatoly Tukhtarov on 1/29/14.
+//  Copyright (c) 2014 Anatoly Tukhtarov. All rights reserved.
+//
+
+#import "GCEditorViewController.h"
+
+@interface GCEditorViewController ()
+
+@end
+
+@implementation GCEditorViewController
+
+@end
