4ac66cbc6eb58d25bcf31af19ae77ce1346b45c9
Anatoly Tukhtarov <anatoly.tukhtarov@yalantis.com>
2014-01-29 15:03:50 +0200

    Initial commit

---
+++ cards/cards/GCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GCAppDelegate.h
+//  cards
+//
+//  Created by Anatoly Tukhtarov on 1/29/14.
+//  Copyright (c) 2014 Anatoly Tukhtarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cards/cards/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cards
+//
+//  Created by Anatoly Tukhtarov on 1/29/14.
+//  Copyright (c) 2014 Anatoly Tukhtarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GCAppDelegate class]));
+    }
+}
